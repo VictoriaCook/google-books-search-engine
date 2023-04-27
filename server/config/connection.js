12c76a7aec35bce30ad8b45d7838217318a03c02
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-27 22:12:33 +1000

    update mongoose connection link

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
+
+//mongoDB_URI is the variable in .env which will be the db link in cloud atlas
